89c1bceb7dcfcd4e74a850cf8767ae4d3cbe1aa0
Mateo Tabernero <51258651+Mateotabernero@users.noreply.github.com>
2023-08-22 15:35:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,41 @@ def bees(num_particles, num_steps):
         
     return particles 
 
+def evolution_bees(num_particles, num_steps, num_simulations): 
+    t = 0 
+    times = np.random.exponential(scale = 1, size = num_particles)
+    particles  =  np.zeros((num_particles, 2))
+    particles_ e = [np.zeros((num_particles, 2))]*(num_steps+1)
+    barycenter =  np.zeros(2) 
+
+    for w in range(num_steps): 
+        j = np.argmin(times) 
+        new_t = times[j]
+        
+        particles = [p + step(new_t-t) for p in particles]  
+        
+
+        barycenter = np.mean(particles, axis = 0) 
+        dist_from_barycenter = np.linalg.norm(particles - barycenter) 
+
+        new_particle = particles[j] 
+        
+        j = np.argmin(dist_from_barycenter)
+        new_time = new_t + np.random.exponential(scale = 1) 
+        times[j] =  new_t + np.random.exponential(scale = 1) 
+
+        particles= np.vstack((particles, new_particle[np.newaxis, :])) 
+        times = np.append(times, new_time) 
+
+        j = np.argmin(dist_from_barycenter) 
+        times = np.delete(times, j, axis = 0) 
+        
+        particles = np.delete(particles, j, axis = 0 )    
+
+        particles_e[w+1] = particles
+
+        
+    return particles_e
 
 # I can separate the function that does one simulation and in this function only make a call to that function for each simulation 
 def bees(num_particles, num_steps, num_simulations):
